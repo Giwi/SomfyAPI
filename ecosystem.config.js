fea44fdb20c0a91383341aed0411e05d00429e4b
Giwi <marin.xavier@gmail.com>
2026-01-11 09:42:06 +0100

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: "SomfyAPI",
-            script: "./dist/srv/index.js",
+            script: "./dist/src/index.js",
             instances: 1,
             watch: false,
             exec_mode: "cluster",
